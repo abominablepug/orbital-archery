ca5d9254fae570aef5446418f5d11175ad0c42c5
abominablepug <esnahoum@gmail.com>
2025-12-11 00:24:18 -0500

    added particle spawning

--- main.cpp
+++ main.cpp
@@ -1,54 +1,48 @@
 #include <raylib.h>
+#include "particle.h"
+#include <vector>
 
-class Particle {
-    private:
-    Vector2 position;
-    Vector2 velocity;
-    Vector2 acceleration;
-    int radius;
-    Color color;
+std::vector<Particle> particles;
 
-    public:
-    Particle(Vector2 pos, Vector2 vel, Color col, Vector2 acc = {0, 9.81f}, int rad = 5) {
-        position = pos;
-        velocity = vel;
-        color = col;
-        acceleration = acc;
-        radius = rad;
-    }
+void drawParticles(float dt) {
+	for (size_t i = 0; i < particles.size(); i++) {
+		particles[i].update(dt);
+		particles[i].draw();
+		particles[i].particleCollision(particles, i);
+	}
+}
 
-    void update(float dt) {
-        position.x += velocity.x * dt;
-        position.y += velocity.y * dt;
-        velocity.x += acceleration.x * dt;
-        velocity.y += acceleration.y * dt;
-    }
-
-    void draw() {
-        DrawCircle(position.x, position.y, radius, color);
-    }
+void spawnParticle() {
+	if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
+		Vector2 mousePos = GetMousePosition();
+		Vector2 velocity = { 50.0f, -50.0f };
+		Color color = BLUE;
+		Vector2 acceleration = { 0, 10.8f };
+		int radius = 5;
+		Particle newParticle(mousePos, velocity, color, acceleration, radius);
+		particles.push_back(newParticle);
+	}
 };
 
 int main() {
     
     const int SCREEN_WIDTH = 1920;
     const int SCREEN_HEIGHT = 1080;
-    const int TARGET_FPS = 180;
+    const int TARGET_FPS = 60;
 
-    Particle particle1 = Particle({0, SCREEN_HEIGHT/2}, {100, -100}, BLUE);
-
-    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Projectile Motion Simulation");
+    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Orbital Archery");
     SetTargetFPS(TARGET_FPS);
 
     while (!WindowShouldClose()) {
 
-        float dt = GetFrameTime();
+        float dt = GetFrameTime() * 3;
+		spawnParticle();
 
         BeginDrawing();
 
         ClearBackground(BLACK);
-        particle1.update(dt);
-        particle1.draw();
+
+		drawParticles(dt);
 
         EndDrawing();
     }
